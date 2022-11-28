90d5d377758064e6046380e31e86d516f45f6e3c
Cloud-Native Toolkit <GandhiCloudLab@gmail.com>
2022-11-28 17:39:13 +0530

    nginx update

--- 01-install-aimgr-on-roks/files/common/07-configure-signed-certifications-for-nginx.sh
+++ 01-install-aimgr-on-roks/files/common/07-configure-signed-certifications-for-nginx.sh
@@ -11,54 +11,17 @@ echo "7.1. Delete your AutomationUIConfig instance and quickly re-create it befo
 echo "-----------------------------------"
 
 AUTO_UI_INSTANCE=$(oc get AutomationUIConfig -n $NAMESPACE --no-headers -o custom-columns=":metadata.name")
-IAF_STORAGE=$(oc get AutomationUIConfig -n $NAMESPACE -o jsonpath='{ .items[*].spec.zenService.storageClass }')
-ZEN_STORAGE=$(oc get AutomationUIConfig -n $NAMESPACE -o jsonpath='{ .items[*].spec.zenService.zenCoreMetaDbStorageClass }')
-oc delete -n $NAMESPACE AutomationUIConfig $AUTO_UI_INSTANCE
-
-cat <<EOF | oc apply -f -
-apiVersion: core.automation.ibm.com/v1beta1
-kind: AutomationUIConfig
-metadata:
-  name: $AUTO_UI_INSTANCE
-  namespace: $NAMESPACE
-spec:
-  description: AutomationUIConfig for cp4waiops
-  license:
-    accept: true
-  version: 1.3.7
-  tls:
-    caSecret:
-      key: ca.crt
-      secretName: external-tls-secret
-    certificateSecret:
-      secretName: external-tls-secret
-  storage:
-    class: $IAF_STORAGE
-  zen: true
-  zenService:
-    storageClass: $IAF_STORAGE
-    zenCoreMetaDbStorageClass: $ZEN_STORAGE
-    iamIntegration: true
-EOF
-
-
-echo "-----------------------------------"
-echo "7.2. Replace the existing secret with a secret that contains the AI Manager ingress certificate."
-echo "-----------------------------------"
-
-# Get the certificate and key from AI Manager ingress.
 ingress_pod=$(oc get secrets -n openshift-ingress | grep tls | grep -v router-metrics-certs-default | awk '{print $1}')
 oc get secret -n openshift-ingress -o 'go-template={{index .data "tls.crt"}}' ${ingress_pod} | base64 -d > cert.crt
 oc get secret -n openshift-ingress -o 'go-template={{index .data "tls.key"}}' ${ingress_pod} | base64 -d > cert.key
-
-# Back up the existing secret to a yaml file.
 oc get secret -n $NAMESPACE external-tls-secret -o yaml > external-tls-secret.yaml
-
-# Delete the existing secret.
 oc delete secret -n $NAMESPACE external-tls-secret
+sleep 15
+oc create secret generic -n $NAMESPACE external-tls-secret --from-file=cert.crt=cert.crt --from-file=cert.key=cert.key -o yaml | oc apply -f -
 
-# Create the new secret with the AI Manager ingress certificate. 
-oc create secret generic -n $NAMESPACE external-tls-secret --from-file=cert.crt=cert.crt --from-file=cert.key=cert.key --dry-run=client -o yaml | oc apply -f -
+oc patch AutomationUIConfig $AUTO_UI_INSTANCE -n $NAMESPACE \
+    --type merge \
+    --patch '{"spec": {"tls": {"caSecret": {"key":"ca.crt", "secretName": "external-tls-secret"}, "certificateSecret": { "secretName": "external-tls-secret"}}}}'
 
 # Recreate nginx. The new NGINX pods get the new certificate. It takes a few minutes for the NGINX pods to come back up.
 oc delete pod $(oc get po -n $NAMESPACE |grep ibm-nginx |awk '{print$1}') -n $NAMESPACE
@@ -66,6 +29,7 @@ oc delete pod $(oc get po -n $NAMESPACE |grep ibm-nginx |awk '{print$1}') -n $NA
 # Remove temp files
 rm cert.crt
 rm cert.key
+# rm ca.crt
 rm external-tls-secret.yaml
 
 }
